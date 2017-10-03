27d8962023a3436da4bd17481b8942f45081f927
kwan14 <mendel.kwan@telus.net>
2017-10-03 20:20:10 +0000

    Took HTML sample and created React components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+
 import React, {Component} from 'react';
+import ChatBar from './Chatbar.jsx';
+import MessageList from  './MessageList.jsx';
+import Message from './Message.jsx';
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <nav className="navbar">
+          <a href="/" className="navbar-brand">Chatty</a>
+        </nav>
+        <main className="messages">
+          <Message />
+          <MessageList />
+        </main>
+        <ChatBar />
+      </div>
     );
   }
 }
